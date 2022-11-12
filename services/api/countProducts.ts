1adec9d47f880772d74d528a059a841a4aef0919
axseinga <urbanowicz.uk@gmail.com>
2022-11-12 15:25:13 +0000

    fix: countProducts type error

--- services/api/countProducts.ts
+++ services/api/countProducts.ts
@@ -1,9 +1,11 @@
 import { fetchProducts } from "./fetchProducts";
   
-export const countProducts = async (count = 0): Promise<number> => {
+export const countProducts = async (count = 0): Promise<number | undefined> => {
     const productsPerFetch = 500;
   
     const products = await fetchProducts(productsPerFetch, count);
+
+    if (!products) return;
   
     const newCount = count + products.length;
   
