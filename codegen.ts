31e0ea3c72ee892a88571ae7c75172a6ce20b2a5
axseinga <urbanowicz.uk@gmail.com>
2022-11-20 17:50:33 +0000

    chore: add date validation fo checkout form, separate graphql mutation and queries

--- codegen.ts
+++ codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 const config: CodegenConfig = {
   overwrite: true,
   schema: "https://api-eu-west-2.hygraph.com/v2/cla4ant1k3ggm01ui9s8ge08e/master",
-  documents: "services/graphql/queries/*.graphql",
+  documents: ["services/graphql/queries/*.graphql", "services/graphql/mutations/*.graphql"],
   generates: {
     "generated/graphql.ts": {
       plugins: [
--- utils/validations/checkoutFormSchema.ts
+++ utils/validations/checkoutFormSchema.ts
@@ -5,6 +5,6 @@ export const checkoutFormSchema = yup.object({
     lastName: yup.string().required("This field cannot be empty").min(3, "Surname has to be at least 3 characters long"),
     email: yup.string().required("This field cannot be empty").min(6, "Email has to be at least 3 characters long").email("This field has to be valid email"),
     cardNumber: yup.string().required("This field cannot be empty").min(16, "Card number has to be at least 16 digits").max(19, "Card number has to be max 19 digits"),
-    expiryDate: yup.string().required("This field cannot be empty"),
+    expiryDate: yup.date().required("This field cannot be empty").min(new Date(), "Expiry date cannot be in the past"),
     CVVNumber: yup.string().required("This field cannot be empty").min(3, "CVV number has to be 3 digits").max(3, "CVV number has to be 3 digits")
   });
